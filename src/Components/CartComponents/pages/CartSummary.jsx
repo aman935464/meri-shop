b0f6fcdac45d64bb3b29ee907ba84c31253e1c08
aman935464 <aman9354644145@gmail.com>
2026-05-02 15:00:47 +0530

    cart page some features added

--- src/Components/CartComponents/pages/CartSummary.jsx
+++ src/Components/CartComponents/pages/CartSummary.jsx
@@ -17,40 +17,43 @@ export default function CartSummary() {
 
   return (
     <>
-      <h1 className="w-[250px] ml-[30px]">Price Details</h1>
+      <h1>Price Details</h1>
       <div id="cart-summary">
-        <div>
+        <div className="section">
           <h3>Price ({totalQuantity} items)</h3>
           <h4>
             {cart[0]?.currency}
             {subTotal}
           </h4>
         </div>
-        <div>
+        <div className="section">
           <h3>Discount ({discountRate * 100}%)</h3>
           <h4>
             -{cart[0]?.currency}
             {discount}
           </h4>
         </div>
-        <div>
+        <div className="section">
           <h3>Tax ({taxRate * 100}%)</h3>
           <h4>
             {cart[0]?.currency}
             {tax}
           </h4>
         </div>
-        <div>
+        <div className="section">
           <h3>Total Amount</h3>
-          <div>
-            <h4>
-            {cart[0]?.currency}
-            {subTotal}
-          </h4>
+          <div className="sec2">
           <h4>
             {cart[0]?.currency}
             {totalAmount}
           </h4>
+          <pre>
+            <p>M.R.P.: </p>
+            <h4 className="pre-price">
+            {cart[0]?.currency}
+            {subTotal}
+          </h4>
+          </pre>
           </div>
         </div>
       </div>
