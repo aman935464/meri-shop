28a5b1b7f6c8b504a59943793252dabb802e5e05
aman935464 <aman9354644145@gmail.com>
2026-05-20 11:33:00 +0530

    added server deploy port

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://meri-shop-server.onrender.com/api",
+  baseURL: "https://meri-shop-ser.onrender.com/api",
   withCredentials: true
 })
 
