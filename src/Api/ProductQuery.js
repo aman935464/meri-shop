d0ce136387eb7a8d7f98de23fd551d33f37c118d
aman935464 <aman9354644145@gmail.com>
2026-06-06 18:02:01 +0530

    Remove old code and update files

--- src/Api/ProductQuery.js
+++ src/Api/ProductQuery.js
@@ -1,11 +1,10 @@
-import { Axios } from "axios";
 import Api from "./Api.js";
 import { useQuery } from "@tanstack/react-query";
 
 const ProductApi = async () => {
-  const res = await Axios.get("https://mocki.io/v1/2ff45da8-562b-41f7-9aeb-fcf84784e1a2");
+  const res = await Api.get("/products");
   
-  return res.data;
+  return res.data.products;
 };
 
 
