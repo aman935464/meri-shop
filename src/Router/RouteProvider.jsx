a9e4cae0563468cd4e3b7b55d7f112f8b24cc42d
aman935464 <aman9354644145@gmail.com>
2026-06-08 00:26:51 +0530

    added protected route

--- src/Router/RouteProvider.jsx
+++ src/Router/RouteProvider.jsx
@@ -42,9 +42,7 @@ const Router = createBrowserRouter([
       {
         path: "product/:id",
         element: (
-          <ProtectedRoute>
             <ProductPage />
-          </ProtectedRoute>
         ),
       },
       {
